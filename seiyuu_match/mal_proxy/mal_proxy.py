400c2d8f3a34f712fbe4516f16ec1e9cda4f1bfb
Justin Shetty <jas0504@gmail.com>
2022-07-17 19:53:18 -0700

    remove unnecessary CORS header

--- seiyuu_match/mal_proxy/mal_proxy.py
+++ seiyuu_match/mal_proxy/mal_proxy.py
@@ -10,8 +10,6 @@ def mal_proxy(route):
     url = MAL_BASE_ADDRESS + '/' + route
     headers = {'X-MAL-CLIENT-ID': seiyuu_match.app.config['MAL_CLIENT_ID']}
     request = requests.get(url, stream=True, params=flask.request.args, headers=headers)
-    response = flask.Response(flask.stream_with_context(request.iter_content()),
-                              content_type=request.headers['content-type'],
-                              status=request.status_code)
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    return response
+    return flask.Response(flask.stream_with_context(request.iter_content()),
+                          content_type=request.headers['content-type'],
+                          status=request.status_code)
